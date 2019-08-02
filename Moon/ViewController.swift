97c38060eb29bc29a73d05c6a6a6fa99078d2e8a
kulaneren <kulaneren@gmail.com>
2019-08-02 18:51:50 +0100

    first commit with 3 option version

--- Moon/ViewController.swift
+++ Moon/ViewController.swift
@@ -13,6 +13,9 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var viewButtonContainer: UIView!
+    let sphere = SCNSphere(radius: 0.2)
+    var lastTag: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,19 +26,37 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//
+//        // Set the scene to the view
+//        sceneView.scene = scene
         
-        // Set the scene to the view
-        sceneView.scene = scene
+//        let sphere = SCNSphere(radius: 0.2)
+        
+        let material = SCNMaterial()
+        
+        material.diffuse.contents = UIImage(named: "art.scnassets/8k_moon.jpg")
+        
+        sphere.materials = [material]
+        
+        let node = SCNNode()
+        
+        node.position = SCNVector3(0, 0, -0.5)
+        
+        node.geometry = sphere
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        sceneView.autoenablesDefaultLighting = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
+        // Create a session configuration        
+        let configuration = ARWorldTrackingConfiguration.isSupported ? ARWorldTrackingConfiguration() : AROrientationTrackingConfiguration()
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -70,6 +91,32 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+    
+    @IBAction func btnClicked(sender: UIButton) {
+        print(sender.tag)
+        if sender.tag == lastTag{
+            return
+        }else{
+            lastTag = sender.tag
+            
+            if let material = sphere.materials.first{
+                switch sender.tag{
+                case 0:
+                    material.diffuse.contents = UIImage(named: "art.scnassets/8k_moon.jpg")
+                case 1:
+                    material.diffuse.contents = UIImage(named: "art.scnassets/8k_mars.jpg")
+                case 2:
+                    material.diffuse.contents = UIImage(named: "art.scnassets/8k_jupiter.jpg")
+                default:
+                    return
+                }
+                sphere.materials = [material]
+            }
+           
+            
+        }
         
     }
 }
